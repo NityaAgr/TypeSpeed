31f680094dd5d8e6f5a5b4d744fcdf8445d3b4e0
Nitya Agrawal <nityajsr24@gmail.com>
2019-03-30 13:17:40 +0530

    Updated on click event

--- src/app/components.paragraph/component.paragraph.ts
+++ src/app/components.paragraph/component.paragraph.ts
@@ -4,19 +4,25 @@ import { Component } from '@angular/core';
   selector: 'paragraph',
   templateUrl: './component.paragraph.html',
   //styleUrls: ['./app.component.css']
+
+
+
+
 })
 export class ParagraphComponent{
-   const i = 0;
-   const txt = 'Lorem ipsum dummy text blabla.';
-   const speed = 100;
 
-function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("demo").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
+
+  i = 0;
+  txt = 'Lorem ipsum dummy text blabla.';
+  speed = 100;
+  typeWriter() {
+    console.log("Button click working");
+    if (this.i < this.txt.length) {
+      document.getElementById("demo").innerHTML += this.txt.charAt(this.i);
+      this.i++;
+      setTimeout(this.typeWriter, this.speed);
+    }
   }
-}
+   
   title = 'Project2';
 }
-     
